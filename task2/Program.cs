e54b1fe6f3c1165a21a6cf6ac63591b82735e8e8
Belek23 <beshik2013@yandex.ru>
2024-02-08 17:23:38 +0300

    add Seminar3:task2

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив из 10 элелментов, заполненный числами из 
+// промежутка [-10, 10]. Замените отрицательные элелменты на 
+// положитлеьные, а положительные на отрицательные.
+
+// Пример
+// [1 -5 6] => [-1 5 -6]
+
+const int size = 10;
+int[] array = new int[size] { 1, -5, 6, 3, 6, 10, -10, -2, 5, -6 };
+
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write(array[i] + " ");
+}
+System.Console.WriteLine();
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = -array[i];
+}
+for (int i = 0; i < size; i++)
+{
+    System.Console.Write(array[i] + " ");
+}
